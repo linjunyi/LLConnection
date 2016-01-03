2fed366889e787438355802f3284381eb9999811
林君毅 <522526435@qq.com>
2016-01-03 20:34:06 +0800

    大文件下载

--- LLConnection/LLDownloaderOperation.m
+++ LLConnection/LLDownloaderOperation.m
@@ -14,12 +14,14 @@
 @property (nonatomic, copy)   LLDownloaderBlock     completionBlock;
 @property (nonatomic, copy)   LLProgressBlock       progressBlock;
 @property (nonatomic, strong) NSURLConnection       *connection;
+@property (nonatomic, assign) NSInteger             currentLength;
 @property (nonatomic, strong) NSNumber              *totalLength;
 @property (nonatomic, strong) NSMutableData         *downloadData;
 @property (nonatomic, copy)   NSString              *downloadPath;
 @property (nonatomic, strong) NSFileHandle          *writeHandle;
 @property (nonatomic, copy)   NSString              *fileName;
 @property (nonatomic, assign) BOOL                  finish;
+@property (nonatomic, assign) BOOL                  isBigFile;
 
 @end
 
@@ -44,16 +46,23 @@
     self.downloadPath  = [NSString stringWithFormat:@"%@/%@", cacheDir, self.fileName];
     NSFileManager *fm = [NSFileManager defaultManager];
     if ([fm fileExistsAtPath:self.downloadPath]) {
-        self.downloadData = [NSMutableData dataWithContentsOfFile:self.downloadPath];
+        NSDictionary *fmAttributes = [fm attributesOfItemAtPath:self.downloadPath error:nil];
+        NSNumber *fileSize = fmAttributes[NSFileSize];
+        if (fileSize) {
+            self.currentLength = fileSize.integerValue;
+        }else {
+            self.currentLength = 0;
+        }
     }else {
+        self.currentLength = 0;
         [fm createFileAtPath:self.downloadPath contents:nil attributes:nil];
     }
     self.writeHandle   = [NSFileHandle fileHandleForWritingAtPath:self.downloadPath];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:self.url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:20.0];
-    NSString *range = [NSString stringWithFormat:@"bytes=%ld-", self.downloadData.length];//用于实现断点续传
+    NSString *range = [NSString stringWithFormat:@"bytes=%u-", self.currentLength];//用于实现断点续传
     [request setValue:range forHTTPHeaderField:@"Range"];                                 //
     self.connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-    [self.connection start];
+    [self.connection start];//异步操作，直到所在线程的同步操作执行完后，再执行
     CFRunLoopRun(); //启动线程的runloop，使线程不离开start方法，否则connection的delegate方法不会被调用，因为start方法结束，对象的生命周期即终止
     if (!self.finish) {
         NSLog(@"Download Fail");
@@ -80,6 +89,12 @@
             if ([httpResponse respondsToSelector:@selector(allHeaderFields)]) {
                 NSDictionary *allHeader = [httpResponse allHeaderFields];
                 self.totalLength = [allHeader objectForKey:@"Content-Length"];
+                if (self.totalLength.longLongValue > 5000) {
+                    self.isBigFile = YES;
+                }else {
+                    self.isBigFile = NO;
+                    self.downloadData = [NSMutableData dataWithContentsOfFile:self.downloadPath];
+                }
             }
         }
     }
@@ -87,10 +102,13 @@
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
     if (self.totalLength) {
-        [self.downloadData appendData:data];
+        if (!self.isBigFile) {
+            [self.downloadData appendData:data];
+        }
         [self.writeHandle seekToEndOfFile];
         [self.writeHandle writeData:data];
-        float progress = ((float)(self.downloadData.length) / self.totalLength.floatValue * 100.0);
+        self.currentLength += data.length;
+        float progress = ((float)(self.currentLength) / self.totalLength.floatValue * 100.0);
         self.progressBlock(progress);
     }
 }
--- LLConnection/LLViewController.m
+++ LLConnection/LLViewController.m
@@ -23,7 +23,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.imageView];
-    LLDownloaderManager *manager = [[LLDownloaderManager alloc] initWithURL:[NSURL URLWithString:@"http://ww1.sinaimg.cn/bmiddle/b8249f32gw1e79qibjfqvg20b405gwpn.gif"] fileName:@"image010.jpg" delegate:self];
+    LLDownloaderManager *manager = [[LLDownloaderManager alloc] initWithURL:[NSURL URLWithString:@"http://60.220.196.206/ws.cdn.baidupcs.com/file/1fcf3c475e9efdbca9ed6375e7a1065a?bkt=p2-nj-981&xcode=9998799de88d2fd620076c6c6d18826f1a2d5db9287d18fdea3971ac1f4f1a6a&fid=840488649-250528-458720856398431&time=1451823244&sign=FDTAXGERLBH-DCb740ccc5511e5e8fedcff06b081203-DnJvUW%2BpUYlrcEAbgFH7U9%2BUbfg%3D&to=lc&fm=Nin,B,U,nc&sta_dx=1214&sta_cs=9729&sta_ft=ISO&sta_ct=7&fm2=Ningbo,B,U,nc&newver=1&newfm=1&secfm=1&flow_ver=3&pkey=14001fcf3c475e9efdbca9ed6375e7a1065a1a06fc2800004bda0000&sl=74711118&expires=8h&rt=pr&r=943210795&mlogid=65939787696508539&vuk=840488649&vbdid=3880713435&fin=MicroSoft%20Office%202011%20Mac.ISO&fn=MicroSoft%20Office%202011%20Mac.ISO&slt=pm&uta=0&rtype=1&iv=0&isw=0&dp-logid=65939787696508539&dp-callid=0.1.1&wshc_tag=0&wsts_tag=5689108d&wsid_tag=72f0fd3f&wsiphost=ipdbm"] fileName:@"02.ISO" delegate:self];
     [manager download];
 }
 
